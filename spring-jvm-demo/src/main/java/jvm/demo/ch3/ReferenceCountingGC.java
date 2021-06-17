989aec54ba220b6bd44b0b5914604a2dfaa693f1
xccu <264672790@qq.com>
2021-06-17 22:04:55 +0800

    added ch3/ReferenceCountingGC.java

---
+++ spring-jvm-demo/src/main/java/jvm/demo/ch3/ReferenceCountingGC.java
@@ -0,0 +1,33 @@
+package jvm.demo.ch3;
+
+/**
+ * 3-1 引用计数算法的缺陷
+ *
+ * Result:
+ * java.lang.OutOfMemoryError: Java heap space
+ * Dumping heap to java_pid10236.hprof ...
+ * Heap dump file created [28245636 bytes in 0.088 secs]
+ *
+ */
+public class ReferenceCountingGC {
+
+    public Object instance = null;
+
+    private static final int _1MB = 1024*1024;
+
+    //该成员属性的唯一意义就是占点内存，以便能在GC日志中看清是否回收过
+    private byte[] bigSize = new byte[2*_1MB];
+
+    public static void main(String[] agrs){
+        ReferenceCountingGC objA = new ReferenceCountingGC();
+        ReferenceCountingGC objB = new ReferenceCountingGC();
+        objA.instance = objB;
+        objB.instance = objA;
+
+        objA=null;
+        objB=null;
+
+        //假设在这行发生GC，观察objA和objB能否被回收
+        System.gc();
+    }
+}
