ca40a37c598915e4f699c918d85b2cfbc4647d88
xccu <264672790@qq.com>
2021-07-08 20:28:36 +0800

    added SynAddRunable.java

---
+++ spring-jvm-demo/src/main/java/jvm/demo/ch4/SynAddRunable.java
@@ -0,0 +1,29 @@
+package jvm.demo.ch4;
+
+/**
+ * 4-9 死锁代码样例
+ * 线程死锁等待演示
+ */
+public class SynAddRunable implements Runnable{
+    int a,b;
+    public SynAddRunable(int a,int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public void run() {
+        synchronized(Integer.valueOf(a)){
+            synchronized (Integer.valueOf(b)){
+                System.out.println(a+b);
+            }
+        }
+    }
+
+    public static void main(String[] agrs) throws Exception {
+        for(int i=0;i<100;i++){
+            new Thread(new SynAddRunable(1,2)).start();
+            new Thread(new SynAddRunable(2,1)).start();
+        }
+    }
+}
